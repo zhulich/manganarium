c373e9bff1ae7acc34c8581fab4c1e066bd23c26
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-11 06:16:37 +0200

    Create 4 classes in models.py

--- ManganariumProject/settings.py
+++ ManganariumProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog'
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "catalog.Translator"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Translator(AbstractUser):
+    language = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username}({self.language})"
+
+
+class Manga(models.Model):
+    title = models.CharField(max_length=63, unique=True)
+    mangaka = models.CharField(max_length=255)
+    year = models.DateField(validators=[MinValueValidator(1950), MaxValueValidator(2023)])
+    chapters = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    genre = models.ManyToManyField(Genre, related_name="genres")
+    translator = models.ManyToManyField(Translator, related_name="translators")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f"{self.title}, {self.year}"
+
+
+class TranslatedManga(models.Model):
+    translated_title = models.CharField(max_length=255, unique=True)
+    original_title = models.ForeignKey(to=Manga, on_delete=models.SET("unknown"), related_name="original", null=True)
+    translator = models.ForeignKey(to=Translator, on_delete=models.SET("unknown"), related_name="translator", null=True)
+
+    def __str__(self):
+        return self.translated_title
+
+
