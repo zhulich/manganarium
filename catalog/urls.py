5082d30773975024e43183c52b9e02a6bd549c80
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-12 00:38:41 +0200

    Add path to Home page and genres

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from catalog.views import index, GenreListView
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("genres/", GenreListView.as_view(), name="genre-list"),
+]
+
+app_name = "catalog"
