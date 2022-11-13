3faa1db8e21a3b36239be630589f6102f2aaaa9d
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-14 00:57:30 +0200

    Preparing for deploy

--- ManganariumProject/settings.py
+++ ManganariumProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-cm#gvy#zf@d8oj3t5g$6%fd74+ujzd_bvk#tk)mz1i^d1^-wa("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
