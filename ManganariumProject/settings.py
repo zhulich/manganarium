166fde693cfa02e7d08285100b72b45aaa5ec3d2
Yevhenii Davydenko <akazhulich@gmail.com>
2022-11-14 05:24:58 +0200

    Remove Django secret key from settings, change DEBUG

--- ManganariumProject/settings.py
+++ ManganariumProject/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-cm#gvy#zf@d8oj3t5g$6%fd74+ujzd_bvk#tk)mz1i^d1^-wa(")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
